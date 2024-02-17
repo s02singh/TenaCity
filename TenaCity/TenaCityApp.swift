7ffa26ae4529ee450f88c2e60542aa2142bc135a
s02singh <sahilbirsingh221@gmail.com>
2024-02-16 21:33:06 -0800

    App setup

---
+++ TenaCity/TenaCityApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenaCityApp.swift
+//  TenaCity
+//
+//  Created by Sahilbir Singh on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TenaCityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
