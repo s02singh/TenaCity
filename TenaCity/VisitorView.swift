6e18bb6a799e317da54fb5696d371d07c8bbd568
lenaray <sonalibhattacharjeeca@gmail.com>
2024-02-22 21:43:41 -0800

    added splash screen

---
+++ TenaCity/VisitorView.swift
@@ -0,0 +1,18 @@
+//
+//  VisitorView.swift
+//  TenaCity
+//
+//  Created by Lena Ray on 2/22/24.
+//
+
+import SwiftUI
+
+struct VisitorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VisitorView()
+}
