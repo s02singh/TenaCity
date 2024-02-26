36bd34f4a934dc50b715233d791c95d2eb6d696a
goldenfries100 <divyaraj940@gmail.com>
2024-02-26 13:55:17 -0800

    icon update

--- TenaCity/View/NavigationBar.swift
+++ TenaCity/View/NavigationBar.swift
@@ -9,33 +9,37 @@ import SwiftUI
 
 struct NavigationBar: View {
     var body: some View {
-        VStack {
-            HStack {
-                Button {
-                    //link to view
-                } label: {
-                    Text("􁽇")
+        NavigationStack {
+            VStack {
+                HStack {
+                    Button {
+                        //link to view
+                    } label: {
+                        Text("􁽇")
+                    }
+                    
+                    Button {
+                        //link to view
+                    } label: {
+                        Text("􀉫")
+                    }
+                    
+                    Button {
+                        //link to view
+                    } label: {
+                        Image("􀝒")
+                    }
+                    
+                    Button {
+                        //link to view
+                    } label: {
+                        Image("􀣋")
+                    }
                 }
-                
-                Button {
-                    //link to view
-                } label: {
-                    Text("􀉫")
-                }
-                
-                Button {
-                    //link to view
-                } label: {
-                    Image("􀝒")
-                }
-                
-                Button {
-                    //link to view
-                } label: {
-                    Image("􀣌")
-                }
+                .background(Color("SageGreen"))
             }
         }
+        .navigationBarBackButtonHidden() //remove top nav bar
     }
 }
 
