c850ce124faa49b87cab1620793ce7c54f9e047f
Shikhar Gupta <shikharscout@gmail.com>
2024-02-28 21:06:00 -0800

    Finished setting up buildingview + authmanager user variables

---
+++ TenaCity/BuildingView.swift
@@ -0,0 +1,18 @@
+//
+//  BuildingView.swift
+//  TenaCity
+//
+//  Created by Shikhar Gupta on 2/28/24.
+//
+
+import SwiftUI
+
+struct BuildingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BuildingView()
+}
