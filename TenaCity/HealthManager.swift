ce05ff745764978fba81163a13fd11b51d067e35
Shikhar Gupta <shikharscout@gmail.com>
2024-02-29 23:42:26 -0800

    finished up adding user's buildings to buildingview and started integration of healthkit

---
+++ TenaCity/HealthManager.swift
@@ -0,0 +1,18 @@
+//
+//  HealthManager.swift
+//  TenaCity
+//
+//  Created by Shikhar Gupta on 2/29/24.
+//
+
+import SwiftUI
+
+struct HealthManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HealthManager()
+}
