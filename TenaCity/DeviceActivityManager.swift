111359b8c4cc080ca7fa8c0179d9613e1f555ace
Shikhar Gupta <shikharscout@gmail.com>
2024-03-07 23:42:40 -0800

    added option to create habits, testing out screen time

---
+++ TenaCity/DeviceActivityManager.swift
@@ -0,0 +1,18 @@
+//
+//  DeviceActivityManager.swift
+//  TenaCity
+//
+//  Created by Shikhar Gupta on 3/7/24.
+//
+
+import SwiftUI
+
+struct DeviceActivityManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeviceActivityManager()
+}
