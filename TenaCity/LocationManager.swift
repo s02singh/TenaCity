55e5c0682502f5e3bf3c8b4bf60afbb31e2a9951
Shikhar Gupta <shikharscout@gmail.com>
2024-03-10 16:48:30 -0700

    set up locations and building views

---
+++ TenaCity/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  TenaCity
+//
+//  Created by Shikhar Gupta on 3/10/24.
+//
+
+import Foundation
---
+++ TenaCity/Views/CreateHabitView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateHabitView.swift
+//  TenaCity
+//
+//  Created by Shikhar Gupta on 3/10/24.
+//
+
+import SwiftUI
+
+struct CreateHabitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateHabitView()
+}
