8246c1f7949d33d5586886e1d4bbf80cf19c5e48
goldenfries100 <divyaraj940@gmail.com>
2024-03-22 11:22:21 -0700

    Info Sheet (not done yet)

--- TenaCity/Views/Home.swift
+++ TenaCity/Views/Home.swift
@@ -89,13 +89,84 @@ struct Home: View {
 
 struct InfoSheet: View {
     var body: some View {
-        Text("This is the info sheet")
-            .padding()
+        ScrollView {
+            VStack {
+                Text("Welcome to TenaCity!")
+                    .padding()
+                    .bold()
+                    .font(.largeTitle)
+                
+                Text("Create a Personal Habit")
+                    .font(.title)
+                    .frame(alignment: .leading)
+                //steps
+                VStack(alignment: .leading) {
+                    HStack() {
+                        Text("1. Navigate to the Building View")
+                        Image(systemName: "building.2")
+                    }
+                    Text("2. Select 'Create Habit'")
+                    Text("3. Select your Habit type")
+                    Text("4. Select your Goal amount for your Habit")
+                    Text("5. Select the building you want to represent your Habit")
+                    Text("6. Select 'Save'")
+                    Text("7. Start working on your Habit!")
+                    
+                }
+                .padding()
+                
+                Text("Update Your Account Info")
+                    .font(.title)
+                    .frame(alignment: .leading)
+                //steps
+                VStack(alignment: .leading) {
+                    HStack() {
+                        Text("1. Navigate to the Settings View")
+                        Image(systemName: "gearshape")
+                    }
+                    Text("2. Select the field that you'd like to update")
+                    Text("NOTE: You are NOT allowed to edit the email field at this time.")
+                    Text("3. Enter your desired information")
+                    Text("4. Select 'Save'")
+                }
+                .padding()
+                
+                Text("Add a Friend")
+                    .font(.title)
+                    .frame(alignment: .leading)
+                //steps
+                Text("1. alsdkfjaldf")
+                    .frame(alignment: .center)
+                
+                Text("Create a Group Habit")
+                    .font(.title)
+                    .frame(alignment: .leading)
+                //steps
+                VStack(alignment: .leading) {
+                    Text("There are two ways that you can create a Group Habit:")
+                    
+                    HStack() {
+                        Text("1. Through the Friends View")
+                        Image(systemName: "person.2")
+                        Text("\n")
+                    }
+                    VStack() {
+                        Text("a. Select the 'Group' button next to the friend that you'd like to create a Habit with")
+                        Text("b. Enter the name of your Habit")
+                    }
+                    .padding(.leading, 20)
+                    
+                }
+                .padding()
+            }
+            .frame(alignment: .center)
+            
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        Home()
+        InfoSheet()
     }
 }
